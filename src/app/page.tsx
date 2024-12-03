e5fba3f9cd31f2fec092d4f9b94e3936c45bf479
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-03 16:43:43 +0100

    Changes page layout so that it matches solution. i.e centering elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const testimonialData = {
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-8 flex items-start justify-center">
+    <main className="min-h-screen p-8 flex items-center justify-center">
       <TestimonialCard
         name={testimonialData.name}
         username={testimonialData.username}
